0e954a60178213578f6bd0f1da81a92e3592443f
Z-C-Lee <1374659280@qq.com>
2021-05-16 15:51:33 +0800

    实现 10折CV验证

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,43 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         ReadData.readExcel();
-        double[] weight = Tools.calWeight(100, ReadData.getLabel().length);
-        Algorithm temp = new Algorithm(5, weight, 0, "rbf", 2, 200);
-        temp.fit(ReadData.getData(),ReadData.getLabel());
+        Tools.splitData(ReadData.getData(),ReadData.getLabel(),10);
 
-//        System.out.println(Arrays.toString(temp.getAlpha()));
-        int right = 0;
-        for (int i = 0; i < ReadData.getData().length; i++) {
-            int predict = temp.predict(ReadData.getData()[i]);
-            if (predict == ReadData.getLabel()[i])right++;
+        for (int i = 0; i < 10; i++) {
+            int per = ReadData.getData().length / 10;
+            int att = ReadData.getData()[0].length;
+            double[][] trainData = new double[per * 9][att];
+            double[] trainLabel = new double[per * 9];
+            double[][] testData;
+            double[] testLabel;
+            testData = Tools.sDatas[i];
+            testLabel = Tools.sTargets[i];
+
+            int num = 0;
+            for (int j = 0; j < 10; j++) {
+                if (j != i){
+                    for (int k = 0; k < per; k++) {
+                        trainData[num] = Tools.sDatas[j][k];
+                        trainLabel[num] = Tools.sTargets[j][k];
+                        num++;
+                    }
+                }
+            }
+
+            double[] weight = Tools.calWeight(100,trainData,trainLabel,false);
+            Algorithm temp = new Algorithm(10, weight, 1e-3, "rbf", 2, 50);
+            temp.fit(trainData, trainLabel);
+
+            int right = 0;
+            for (int k = 0; k < testData.length; k++) {
+                int predict = temp.predict(testData[k]);
+                if (predict == testLabel[k]) right++;
+            }
+            System.out.println(right / 25.0);
         }
-        System.out.println(right);
+
     }
 }
--- src/Tools.java
+++ src/Tools.java
@@ -1,9 +1,52 @@
-public class Tools {
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Random;
 
-    public static double[] calWeight(int paraC, int len){
+public class Tools {
+    //划分后的数据
+    public static double[][][] sDatas;
+    //划分后的标签
+    public static double[][] sTargets;
+    /**
+     * 划分数据集
+     * @param data 待划分数据
+     * @param target 待划分标签
+     * @param KFold 份数
+     */
+    public static void splitData(double[][] data, double[] target, int KFold){
+        int per = data.length / KFold;
+        int att = data[0].length;
+        sDatas = new double[KFold][per][att];
+        sTargets = new double[KFold][per];
+
+        Random rand = new Random();
+        LinkedList<Double> currentTarget = new LinkedList<>();
+        LinkedList<double[]> currentData = new LinkedList<>(Arrays.asList(data));
+        for(double temp:target)currentTarget.add(temp);
+
+        for (int i = 0; i < KFold; i++) {
+            for (int j = 0; j < per; j++) {
+                int site = rand.nextInt(currentTarget.size());
+                sDatas[i][j] = currentData.remove(site);
+                sTargets[i][j] = currentTarget.remove(site);
+            }
+        }
+    }
+
+    /**
+     * 计算样本惩罚项权重
+     * @param paraC 惩罚项系数
+     * @param flag 是否采用模糊支持向量机
+     * @return 样本权重
+     */
+    public static double[] calWeight(int paraC, double[][] data, double[] target, boolean flag){
+        int len = target.length;
         double[] result = new double[len];
         for (int i = 0; i < len; i++) {
             result[i] = paraC;
+        }// end of if
+        if (flag){
+
         }
         return result;
     }
